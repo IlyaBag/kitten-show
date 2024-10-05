bf65264303b4aff022ea1fdab5d279f4ed450cee
Ilya Bagrov <ilya.b.only@gmail.com>
2024-10-05 19:43:04 +0300

    Add helpers for working with the database

--- kitten_show/database/db.py
+++ kitten_show/database/db.py
@@ -1,7 +1,4 @@
-from typing import AsyncGenerator
-
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, \
-create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from config import DB_ECHO, DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
@@ -13,10 +10,3 @@ engine = create_async_engine(
 
 async_session_factory = async_sessionmaker(engine, autoflush=False,
                                            expire_on_commit=False)
-
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    session = async_session_factory()
-    try:
-        yield session
-    finally:
-        await session.close()
---
+++ kitten_show/database/helpers.py
@@ -0,0 +1,27 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.db import async_session_factory
+from database.models import KittenModel
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    session = async_session_factory()
+    try:
+        yield session
+    finally:
+        await session.close()
+
+async def get_kitten_by_id(
+    id: int,
+    session: AsyncSession = Depends(get_db_session)
+) -> KittenModel:
+    kitten = await session.get(KittenModel, id)
+    if not kitten:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Sorry, but the kitten with id={id} was not found'
+        )
+    return kitten
