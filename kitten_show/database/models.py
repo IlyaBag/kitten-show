a1e520dd7bb95ee1cba1ef4565d09bb1c56e2a42
Ilya Bagrov <ilya.b.only@gmail.com>
2024-10-12 17:14:17 +0300

    Add a reminder of a fastapi func function

--- kitten_show/database/models.py
+++ kitten_show/database/models.py
@@ -13,6 +13,7 @@ class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
+        # server_default=func.now(),
         server_default=text("TIMEZONE('utc', now())"),
     )
     updated_at: Mapped[datetime] = mapped_column(
